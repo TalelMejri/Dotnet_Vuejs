2b0027ce0d89dedcf2f9bb65e8b0f405be3b87fa
TalelMejri <talelmejri8@gmail.com>
2024-01-15 13:41:55 +0100

    first commit

---
+++ Backend/Backend/Models/DbContextClasse.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Models
+{
+    public class DbContextClasse : DbContext
+    {
+
+        public DbSet<Todo> Todos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"data source=Desktop-d1jfgmm\sqlexpress; initial catalog=TodosDb; integrated security=SSPI;TrustServerCertificate=True");
+        }
+    }
+}
---
+++ Backend/Backend/Models/Todo.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Backend.Models
+{
+    [Table("todos")]
+    public class Todo
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+
+        [Required]
+        [Column(TypeName ="varchar(150)")]
+        public string? title { get; set; }
+
+        [Required]
+        [Column(TypeName ="varchar(255)")]
+        public string? description { get; set; }
+
+        [Required]
+        public bool status { get; set; }
+
+
+    }
+}
