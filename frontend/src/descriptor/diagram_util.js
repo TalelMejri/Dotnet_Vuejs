f55325a64971839e3b7aa4838c3d309adf2e234f
TalelMejri <talelmejri8@gmail.com>
2024-02-15 23:19:01 +0100

    Organize Functions

--- frontend/src/descriptor/diagram_util.js
+++ frontend/src/descriptor/diagram_util.js
@@ -1,50 +1,39 @@
-import { decompress, compress } from "lz-string";
-import blankDiagram from "../../public/diagram.bpmn";
-
 export function openDiagram(modeler, diagramXml) {
-  console.log(blankDiagram);
-  modeler.importXML(blankDiagram, function(err) {
-    if (!err) {
-      console.log("success!");
-      modeler.get("canvas").zoom("fit-viewport");
-    } else {
-      console.log("something went wrong:", err);
-    }
-  });
-  console.log(modeler.get("canvas"));
-  modeler.get("canvas").zoom("fit-viewport");
+  fetch(diagramXml)
+  .then(response => response.text())
+  .then(diagram => {
+    modeler.importXML(diagram, error => {
+      if (error) {
+        console.error('Could not import BPMN diagram', error);
+      } else {
+        modeler.get('canvas').zoom('fit-viewport');
+      }
+    });
+  })
+  .catch(err => console.error('Error loading BPMN diagram', err));
 }
 
-export function openLocalDiagram(modeler) {
-  /*let localDiagram = sessionStorage.getItem("diagram");
-  let diagram = localDiagram ? decompress(localDiagram) : blankDiagram;*/
-  
-  return openDiagram(modeler, blankDiagram);
-}
 
 export function resetDiagramToBlank(modeler) {
-  return openDiagram(modeler, blankDiagram);
+   openDiagram(modeler, './InitDiagram.bpmn');
 }
 
 export function saveDiagram(modeler) {
-  return new Promise(resolve => {
-    modeler.saveXML({ format: true }, function(err, xml) {
-      sessionStorage.setItem("diagram", compress(xml));
-      resolve(getDownloadUrl(xml));
-    });
-  });
+      modeler.saveXML({ format: true }, function(err, xml) {
+          if (err) {
+            console.log('Error saving XML', err);
+          } else {
+            const blob = new Blob([xml], { type: 'application/bpmn20-xml;charset=utf-8' });
+            const url = window.URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = 'diagram.bpmn';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            window.URL.revokeObjectURL(url);
+          }
+        })
 }
 
-export function getDownloadUrl(xml) {
-  return `data:application/bpmn20-xml;charset=UTF-8,${encodeURIComponent(xml)}`;
-}
 
-export function getBlankDiagramDownloadUrl() {
-  return getDownloadUrl(blankDiagram);
-}
-
-export function parseBPMNJson({ rootElements }) {
-  const steps = rootElements
-    .filter(el => el.$type === "bpmn:Process")
-    .reduce((acc, curr) => curr.flowElements);
-}
