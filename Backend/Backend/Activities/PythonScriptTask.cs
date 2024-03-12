a7016af471d01e5135c2795a32f4b60ce00331ba
TalelMejri <talelmejri8@gmail.com>
2024-03-13 00:35:56 +0100

    fix pythonDll

--- Backend/Backend/Activities/PythonScriptTask.cs
+++ Backend/Backend/Activities/PythonScriptTask.cs
@@ -20,9 +20,9 @@ namespace Backend.Activities
         public string Script { get; set; }
         public void TestPython()
         {
-            Runtime.PythonDLL = @"C:\Users\talel\AppData\Local\Programs\Python\Python312\Python312.dll";
-            PythonEngine.Initialize();
-            PythonEngine.BeginAllowThreads();
+           Runtime.PythonDLL = @"C:\Users\talel\AppData\Local\Programs\Python\Python312\Python312.dll";
+           PythonEngine.Initialize();
+           PythonEngine.BeginAllowThreads();
 
             using (Py.GIL())
             {
